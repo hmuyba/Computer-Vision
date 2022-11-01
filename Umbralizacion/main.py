e6a1ef4888bcd79c30b98d088e9564beb9f4f531
Andres <53712714+Andres-Pereira@users.noreply.github.com>
2022-10-31 22:11:19 -0400

    Added an tested adaptive umbralization

--- Umbralizacion/main.py
+++ Umbralizacion/main.py
@@ -3,24 +3,43 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('water_coins.jpg', 1)
-# cv2.imshow('Foto original', img)
-# cv2.waitKey(0)
 
-u, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-u, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
-u, th3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
-u, th4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
-u, th5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
+def umbralizacion_Simple():
 
-images = [img, th1, th2, th3, th4, th5]
-labels = ['Original','Binary', 'Binary Inv', 'Trunc', 'To zero', 'To zero Inv']
+    img = cv2.imread('libro.jpg', 0)
 
-for i in range(6):
-    plt.subplot(3,2,i+1)
-    plt.imshow(images[i],'gray',vmin=0,vmax=255);
-    plt.title(labels[i])
-    plt.xticks([]), plt.yticks([])
+    u, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    u, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+    u, th3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+    u, th4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+    u, th5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
-plt.show()
-print(u)
+    images = [img, th1, th2, th3, th4, th5]
+    labels = ['Original', 'Binary', 'Binary Inv', 'Trunc', 'To zero', 'To zero Inv']
+
+    for i in range(6):
+        plt.subplot(3, 2, i + 1)
+        plt.imshow(images[i], 'gray', vmin=0, vmax=255);
+        plt.title(labels[i])
+        plt.xticks([]), plt.yticks([])
+
+    plt.show()
+    print(u)
+
+def umbralizacion_Adaptativa():
+
+    img = cv2.imread('libro.jpg', 0)
+    th1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    images = [th1, th2]
+    titles = ['ADAPTIVE_THRESH_MEAN_C', 'ADAPTIVE_THRESH_GAUSSIAN_C']
+    for i in range(2):
+        plt.subplot(1, 2, i + 1)
+        plt.imshow(images[i], 'gray', vmin=0, vmax=255)
+        plt.title(titles[i])
+        plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+umbralizacion_Simple()
