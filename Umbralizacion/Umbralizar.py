7e7fbadbc628938108ccf4c97b2614280d6f6daf
Andres <53712714+Andres-Pereira@users.noreply.github.com>
2022-11-07 17:24:41 -0400

    update comments

--- Umbralizacion/Umbralizar.py
+++ Umbralizacion/Umbralizar.py
@@ -5,9 +5,11 @@ from matplotlib import pyplot as plt
 
 
 def umbralizacion_Demostrativa():
-
+    # 0 para que se lea en escala de grises
     img = cv2.imread('escala.jpg', 0)
 
+    # imagen, umbral o thresh, maxvalue, metodo => umbral e imagen umbralizada
+    # 0 negro, maximo blanco
     u, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
     u, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
     u, th3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
@@ -18,9 +20,11 @@ def umbralizacion_Demostrativa():
     labels = ['Original', 'Binary', 'Binary Inv', 'Trunc', 'To zero', 'To zero Inv']
 
     for i in range(6):
+        # 3 filas, 2 columnas, pos
         plt.subplot(3, 2, i + 1)
         plt.imshow(images[i], 'gray', vmin=0, vmax=255);
         plt.title(labels[i])
+        # ejes que encierran a las figuras
         plt.xticks([]), plt.yticks([])
 
     plt.show()
@@ -30,6 +34,7 @@ def umbralizacion_Demostrativa():
 def umbralizacion_Adaptativa():
 
     img = cv2.imread('caballo.jpg', 0)
+    # imagen, maxvalue, metodo adaptativo, metodo de umbraliz, tam del bloque que tendran los pixeles vecinos, constante
     th1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
     u, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
@@ -44,6 +49,7 @@ def umbralizacion_Adaptativa():
 
 
 def umbralizacion_Automatica():
+
 
     img1 = cv2.imread('gato.jpg', 0)
     img2 = cv2.imread('banana.jpg', 0)
